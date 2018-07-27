01a2c60cfe710be21876dd727f049e4fd936a914
Tom Reinartz <tom.reinartz@dotcontrol.nl>
2018-07-27 13:19:24 +0200

    remove console.log

--- src/util/EpicUtil.js
+++ src/util/EpicUtil.js
@@ -1,6 +1,5 @@
 export class EpicUtil {
 	static onAction(action = null, ...sideEffects) {
-		console.log(sideEffects);
 		return (action$) => action$.ofType(action)
 															 .pipe(...sideEffects);
 	}
